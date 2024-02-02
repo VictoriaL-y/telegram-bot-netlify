1adaddba795fe1e6d3c30814ffdf5b03b811cd4f
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2024-02-03 00:24:30 +0100

    Update the serverless function

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,11 +1,11 @@
 const { handleMessage } = require('../../controller/lib/Telegram');
 
-exports.handler = async (req) => {
+exports.handler = async (req, method) => {
     const { body } = req;
     if (body) {
         const messageObj = body.message;
         await handleMessage(messageObj);
     }
-    return;
+    return await req;
 
 }
