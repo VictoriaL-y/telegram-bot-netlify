f31d2377ceca89fe7d92c30140cde00e2c4664b3
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2024-02-02 23:41:18 +0100

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,8 @@
-exports.handler = async (event) => {
-    console.log("Received an update from Telegram!", event.body);
-    return { statusCode: 200 };
-  };
+const axios = require("axios").default;
+
+await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+    chat_id: JSON.parse(e.body).message.chat.id,
+    text: "I got your message!",
+});
+
+return { statusCode: 200 };
