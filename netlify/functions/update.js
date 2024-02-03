cba144bb36da858c0008056ac5c3e6b964807511
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2024-02-03 01:23:02 +0100

    Update the serverless function

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -3,7 +3,7 @@ const { handleMessage } = require('../../controller/lib/Telegram');
 exports.handler = async (req, method) => {
     const { body } = req;
     if (body) {
-        const messageObj = Json.parse(body).message;
+        const messageObj = JSON.parse(body).message;
         console.log(messageObj);
         console.log(messageObj.text);
         console.log(messageObj.chat_id)
