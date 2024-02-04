87e3154b35067e166f9ec216c96b6d26bf55ed87
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2024-02-04 23:23:44 +0100

    edited mongodb connection and env

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // connect a conversion-table database with this project
-const dbURL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@conversion-table.snruosp.mongodb.net/`;
+const dbURL = `${process.env.MONGODB_URI}`;
 
 mongoose.connect(dbURL)
     .then(console.log("Connected to MongoDB"))
