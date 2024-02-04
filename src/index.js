d5f6caeb86573e99ea845dbb66a679c39e2b4ca5
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2024-02-04 23:57:39 +0100

    edited mongodb connection

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ require("dotenv").config();
 
 // connect a conversion-table database with this project
 
-setTimeout(function () {
-    const connectMongoDB = async () => {
-        try {
-            await mongoose.connect(process.env.MONGODB_URI);
-            console.log("Connected to MongoDB");
-        } catch (error) {
-            console.log(error);
-        }
-    }
-}, 60000);
+const connectMongoDB = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+}
+
+connectMongoDB()
+    .catch((err) => console.error(err));
 
 module.exports = connectMongoDB;
 
